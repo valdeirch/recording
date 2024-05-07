a94e60a3282e32e84545ce86d40c2c5cee14807d
Valdeir <valdeirsbs@gmail.com>
2024-05-06 21:25:32 -0300

    first commit

---
+++ src/app.controller.ts
@@ -0,0 +1,68 @@
+import {
+  BadRequestException,
+  Controller,
+  Get,
+  HttpCode,
+  HttpStatus,
+  Post,
+  Req,
+  UploadedFiles,
+  UseInterceptors,
+} from '@nestjs/common';
+import { AppService } from './app.service';
+import { randomUUID } from 'crypto';
+import { extname } from 'path';
+import { FileFieldsInterceptor } from '@nestjs/platform-express';
+import { diskStorage } from 'multer';
+
+@Controller()
+export class AppController {
+  constructor(private readonly appService: AppService) {}
+
+  @Get()
+  getHello(): string {
+    return this.appService.getHello();
+  }
+
+  @Post('video')
+  @HttpCode(HttpStatus.CREATED)
+  @UseInterceptors(
+    FileFieldsInterceptor(
+      [
+        { name: 'video', maxCount: 1 },
+        { name: 'thumbnail', maxCount: 1 },
+      ],
+      {
+        dest: './uploads',
+        storage: diskStorage({
+          destination: './uploads',
+          filename: (_req, file, cb) => {
+            return cb(
+              null,
+              `${Date.now()}-${randomUUID()}${extname(file.originalname)}`,
+            );
+          },
+        }),
+        fileFilter: (_req, file, cb) => {
+          if (file.mimetype !== 'video/mp4' && file.mimetype !== 'image/jpeg') {
+            return cb(
+              new BadRequestException(
+                'Invalid file type. Only video/mp4 and image/jpeg are supported.',
+              ),
+              false,
+            );
+          }
+          return cb(null, true);
+        },
+      },
+    ),
+  )
+  async uploadVideo(
+    @Req() _req: Request,
+    @UploadedFiles()
+    files: { video?: Express.Multer.File[]; thumbnail?: Express.Multer.File[] },
+  ): Promise<string> {
+    console.log(files);
+    return 'video uploaded';
+  }
+}
